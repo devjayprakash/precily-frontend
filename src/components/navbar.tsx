5974647b0d2f53b66762856d12c2acb30add28f5
Jay prakash pathak <jayprskhpthk@gmail.com>
2021-06-27 14:53:10 +0530

    initial project setup with vite and tailwind css

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <div className={"w-full bg-purple-500 text-white"}>
+      <div className={"px-3 py-2"}>Precily frontend</div>
+    </div>
+  );
+};
+
+export default Navbar;
